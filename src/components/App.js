1ba68b9721710f721a8dd65bc142f4bcaa312431
Richard Frausto <vindigo@gmail.com>
2020-05-29 10:45:41 -0700

    ERROR: no file or directory in postReducer

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PostList from './PostList';
+// import UserHeader from './UserHeader';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  )  
+};
+
+export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostList extends React.Component{
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>Post List</div>
+    )
+  }
+}
+      
+const mapStateToProps = state => {
+  return { posts: state.posts }; 
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
+
+
